75eb7ba65fdbba99ca1fae3e6b8eb37b41d6ee6d
kuanhuachen <82696770+kuanhuachen@users.noreply.github.com>
2023-10-24 16:11:43 +0800

    Update Package.swift
    change version number from 2.2.2 to 2.4.6 in package file

--- Package.swift
+++ Package.swift
@@ -26,22 +26,22 @@ let package = Package(
             dependencies: []),
         .binaryTarget(
             name: "AuthMe",
-            url: "https://storage.googleapis.com/authme-mobile.appspot.com/iOS/2.2.2/AuthMe.xcframework.zip",
+            url: "https://storage.googleapis.com/authme-mobile.appspot.com/iOS/2.4.6/AuthMe.xcframework.zip",
             checksum: "f30fcad3240793f6c6c809f0fc24e578c0daeaa804fae92335cda434ead0a068"
         ),
         .binaryTarget(
             name: "AuthMeUI",
-            url: "https://storage.googleapis.com/authme-mobile.appspot.com/iOS/2.2.2/AuthMeUI.xcframework.zip",
+            url: "https://storage.googleapis.com/authme-mobile.appspot.com/iOS/2.4.6/AuthMeUI.xcframework.zip",
             checksum: "31ff162be6496bca6664c442047cabb2d2907a4207dc9f315c24c567332aa356"
         ),
         .binaryTarget(
             name: "onnxruntime",
-            url: "https://storage.googleapis.com/authme-mobile.appspot.com/iOS/2.2.2/onnxruntime.xcframework.zip",
+            url: "https://storage.googleapis.com/authme-mobile.appspot.com/iOS/2.4.6/onnxruntime.xcframework.zip",
             checksum: "c3ec0a200062bf8e388a94b5200561812731425580ac83529e218016a9e1af5a"
         ),
         .binaryTarget(
             name: "OpenSSL",
-            url: "https://storage.googleapis.com/authme-mobile.appspot.com/iOS/2.2.2/OpenSSL.xcframework.zip",
+            url: "https://storage.googleapis.com/authme-mobile.appspot.com/iOS/2.4.6/OpenSSL.xcframework.zip",
             checksum: "cf1d5849465b111c8db4e1c432b096e8051c3cf018b673c9a3422715fa8abb71"
         ),
     ]
