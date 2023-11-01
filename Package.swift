ce96877e40c1adf76286f118bbe8233a565deee3
kuanhuachen <kuanhua.chen@amazingtalker.com>
2023-11-01 18:01:02 +0800

    fix

--- Package.swift
+++ Package.swift
@@ -4,46 +4,45 @@
 import PackageDescription
 
 let package = Package(
-    name: "AuthMeSPM",
-    products: [
-        .library(
-            name: "AuthMeSPM",
-            targets: [
-                "AuthMeSPM",
-                "AuthMe",
-                "AuthMeUI"
-// ,
-//                 "onnxruntime",
-//                 "OpenSSL"
-            ]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        .target(
-            name: "AuthMeSPM",
-            dependencies: []),
-        .binaryTarget(
-            name: "AuthMe",
-            url: "https://storage.googleapis.com/authme-mobile.appspot.com/iOS/2.4.6/AuthMe.xcframework.zip",
-            checksum: "c72c87df70242825b78383c0bd366b8b19febf57371b9c3d64b07520d7ed1b98"
-        ),
-        .binaryTarget(
-            name: "AuthMeUI",
-            url: "https://storage.googleapis.com/authme-mobile.appspot.com/iOS/2.4.6/AuthMeUI.xcframework.zip",
-            checksum: "ff95c28e9e893568efb3cba6d6cfef11df961c5633b93e131416496e8057438f"
-        ),
-        // .binaryTarget(
-        //     name: "onnxruntime",
-        //     url: "https://storage.googleapis.com/authme-mobile.appspot.com/iOS/2.4.6/onnxruntime.xcframework.zip",
-        //     checksum: "c3ec0a200062bf8e388a94b5200561812731425580ac83529e218016a9e1af5a"
-        // ),
-        // .binaryTarget(
-        //     name: "OpenSSL",
-        //     url: "https://storage.googleapis.com/authme-mobile.appspot.com/iOS/2.4.6/OpenSSL.xcframework.zip",
-        //     checksum: "cf1d5849465b111c8db4e1c432b096e8051c3cf018b673c9a3422715fa8abb71"
-        // ),
-    ]
+  name: "AuthMeSPM",
+  products: [
+    .library(
+      name: "AuthMeSPM",
+      targets: [
+        "AuthMeSPM",
+        "AuthMe",
+        "AuthMeUI",
+        "Algo",
+        "OpenSSL"
+      ]),
+  ],
+  dependencies: [
+    // Dependencies declare other packages that this package depends on.
+     .package(url: "https://github.com/airbnb/lottie-ios", from: 4.3.3),
+  ],
+  targets: [
+    .target(
+      name: "AuthMeSPM",
+      dependencies: []),
+    .binaryTarget(
+      name: "AuthMe",
+      url: "https://storage.googleapis.com/authme-mobile.appspot.com/iOS/2.4.6/AuthMe.xcframework.zip",
+      checksum: "4ff9754d233700aa72793927dbcda1b459e0608f0f50a2c4f39409e261856b23"
+    ),
+    .binaryTarget(
+      name: "AuthMeUI",
+      url: "https://storage.googleapis.com/authme-mobile.appspot.com/iOS/2.4.6/AuthMeUI.xcframework.zip",
+      checksum: "e316811a5f87bdb75dd81803e1b251b11d09dc95b52f2854829db4f909f0c852"
+    ),
+    .binaryTarget(
+      name: "Algo",
+      url: "https://storage.googleapis.com/authme-mobile.appspot.com/iOS/2.4.6/Algo.xcframework.zip",
+      checksum: "4caa724b2d15af56f6d65671df47e28adfe982206205a62889d5ffd68423c2eb"
+    ),
+    .binaryTarget(
+      name: "OpenSSL",
+      url: "https://storage.googleapis.com/authme-mobile.appspot.com/iOS/2.4.6/OpenSSL.xcframework.zip",
+      checksum: "402ddb53a920d584f41ed402d2f032f9df12f04997e5633eb8bd944c16344a0d"
+    ),
+  ]
 )
